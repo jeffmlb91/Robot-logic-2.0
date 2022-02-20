d54f52e754573f3f61f7e167054ecc29dfad6b84
jeffmlb91 <jeffmlb91@gmail.com>
2022-02-20 08:59:08 -0500

    test1

--- asset/js/game.js
+++ asset/js/game.js
@@ -1,1 +1,6 @@
-window.alert("This is an alert! Javascript is running!");
+//window.alert("This is an alert! Javascript is running!");
+
+function fight() {
+    window.alert('The fight has begun')
+}
+ 
